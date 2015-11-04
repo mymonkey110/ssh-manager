f1d96c1d7256b8f68dfd9d68c15913d35379f57f
jiangwk <jiangwk@haoocai.com>
2015-11-04 17:34:24 +0800

    1.Add requirements.txt 2.Fix config.db.schema 3.Add remove function

--- sm.py
+++ sm.py
@@ -26,30 +26,42 @@ def init_config_db():
         print("Initialize completed!")
 
 
-def load_data():
-    with sqlite3.connect(config_db_path) as conn:
-        cursor = conn.cursor()
-        cursor.execute("SELECT id,alias,hostname,password,gmt_create,gmt_modify FROM host")
-        for row in cursor.fetchall():
-            print row
-
-
 def show_host():
+    """
+    Show host entry list
+    :return: None
+    """
     with sqlite3.connect(config_db_path) as conn:
         cursor = conn.cursor()
-        cursor.execute("SELECT id,alias,hostname,password,gmt_create,gmt_modify FROM host")
-        for row in cursor.fetchall():
-            print row
+        cursor.execute("SELECT id,alias,hostname,username FROM host")
+        data = cursor.fetchall()
+        if len(data) > 0:
+            print("id | alias | hostname | username")
+            print("-- | ----- | ------------- | ----")
+            for row in data:
+                _id, alias, hostname, username = row
+                print("%2d | %5s | %15s | %4s" % (_id, alias, hostname, username))
+        else:
+            print("No host found")
 
 
 def add_host(hostname, password, username="root", alias=None):
+    """
+    Add host to db
+    :param hostname: hostname
+    :param password: ssh password
+    :param username: ssh username,default 'root'
+    :param alias: host alias
+    :return: None
+    """
     with sqlite3.connect(config_db_path) as conn:
         cursor = conn.cursor()
         if alias:
             sql = "insert into host(hostname,username,password,alias) values('%s','%s','%s','%s')" % (
                 hostname, username, password, alias)
         else:
-            sql = "insert into host(hostname,username,password) values('%s','%s','%s')" % (hostname, username, password)
+            sql = "insert into host(hostname,username,password) values('%s','%s','%s')" % \
+                  (hostname, username, password)
 
         cursor.execute(sql)
         if cursor.rowcount != 1:
@@ -57,11 +69,30 @@ def add_host(hostname, password, username="root", alias=None):
         cursor.close()
 
 
+def remove_host(alias, _id):
+    if not alias and not _id:
+        print("please assign host alias or id")
+
+    with sqlite3.connect(config_db_path) as conn:
+        cursor = conn.cursor()
+        if alias:
+            cursor.execute("DELETE FROM host WHERE alias=" + alias)
+        else:
+            cursor.execute("DELETE FROM host WHERE id=" + _id)
+
+
 def init():
     if not check_db_exist():
         init_config_db()
-    else:
-        load_data()
+
+
+def show_usage():
+    print("""
+    sm [show|add|remove]
+    show    show host list
+    add     add host to db
+    remove  remove host from db
+    """)
 
 
 if __name__ == "__main__":
@@ -72,3 +103,5 @@ if __name__ == "__main__":
             show_host()
         if sys.argv[1] == "add":
             add_host(hostname=sys.argv[2], username=sys.argv[3], password=sys.argv[4])
+    else:
+        show_usage()
