b6c0779f5bbbaa437886de9759183fae3eeee139
jiangwk <jiangwk@haoocai.com>
2015-11-07 17:36:08 +0800

    1.remove LICENSE 2.Add setup 3.Fix gitignore

--- setup.py
+++ setup.py
@@ -1,1 +1,15 @@
-__author__ = 'Michael Jiang'
+from setuptools import setup, find_packages
+
+setup(
+    name='sm',
+    version='0.0.1',
+    keywords=('ssh', 'manager'),
+    description="ssh manager tool",
+    license='MIT License',
+    install_requires=['ecdsa==0.13', 'pexpect==4.0.1', 'ptyprocess==0.5', 'pycrypto==2.6.1', 'tabulate==0.7.5',
+                      'wheel==0.24.0'],
+    author = 'Michael Jiang',
+    author_email = 'mymonkey110@gmail.com',
+    packages = find_packages(),
+    platform = 'any'
+)
